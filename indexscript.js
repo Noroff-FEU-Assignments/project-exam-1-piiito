5d243fefcc81087a5f54af259e7ddc77d7a6bfbd
piiito <birgittevedaa@hotmail.com>
2022-05-28 23:26:23 +0200

    updates

---
+++ indexscript.js
@@ -0,0 +1,124 @@
+const carousel = document.querySelector(".carousel_posts");
+const mainUrl = "http://localhost/flower-power/wp-json/wp/v2/posts/";
+const queryString = document.location.search;
+let params = new URLSearchParams(queryString);
+const id = params.get("id");
+console.log(id);
+
+
+carouselUrl = mainUrl + id;
+console.log(carouselUrl);
+carousel.innerHTML="Loading..";
+
+async function getProducts(carouselUrl){
+        const response = await fetch(carouselUrl);
+        const results = await response.json();
+        console.log(results);
+    
+    carousel.innerHTML ="";
+    for(let i =0; i <results.length; i++){
+        console.log(results[i].title.rendered);
+       carousel.innerHTML +=`<div class="slide">
+                            <a href="spesificpost.html?id=${results[i].id}"> 
+                            <h2 class="title">${results[i].title.rendered}</h2>
+                            <img ${results[i].content.rendered}
+                            </a>
+                            </div>`;
+                            
+    }
+
+   /* carousel.innerHTML="";
+    results.forEach(function(results){
+        carousel.innerHTML += `<div>
+        <a href="spesificpost.html?id=${results.id}"> 
+        <h1>${results.title.rendered}</h1>
+        ${results.content.rendered}
+        </a>
+        </div>`;
+    })*/
+}
+
+getProducts(mainUrl);
+
+/*async function productDetails(){
+    try{
+        const fetchDetails = await fetch(detailPageUrl);
+        const details = await fetchDetails.json();
+        console.log(details);
+
+        createHtml(details);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+productDetails();
+
+function createHtml(details){
+    detailPage.innerHTML += `<h1>${details.name}</h1>`;
+}*/
+
+
+/*const carouselForward = document.querySelector(".arrow_button_forward");
+
+carouselForward.onclick= function(){
+
+    const carouselUrl = mainUrl + "?per_page=12";
+    carousel.innerHTML = "";
+    getProducts(carouselUrl);
+ 
+}*/ 
+/*
+document.addEventListener("click", e=> {
+let arrow
+if(e.target.matches(".arrow")){
+    arrow = e.target
+} else{
+arrow = e.target.closest(".arrow")
+}
+if (arrow != null) onArrowClick(arrow)
+})
+
+function onArrowClick(arrow){
+    const carouselContainer = arrow.closest(".home_container").querySelector(".carousel_posts")
+    const sliderIndex = parseInt(getComputedStyle(carouselContainer).getPropertyValue("--slider-index"))
+    const itemsPerScreen = parseInt(getComputedStyle(carouselContainer).getPropertyValue("--items-per-screen"))
+    if(arrow.classList.contains("arrow_button_back")){
+        carouselContainer.style.setProperty("--slider-index", sliderIndex - 1)
+    }
+    if (arrow.classList.contains("arrow_button_forward")){
+        carouselContainer.style.setProperty("--slider-index", sliderIndex + 1)
+    }
+    
+carousel
+}*/
+
+const slides = document.querySelectorAll(".carousel_posts");
+const forwardButton = document.querySelector(".arrow_button_forward");
+const backButton = document.querySelector(".arrow_button_back");
+
+slides.forEach(function(slide, index){
+    slide.style.left = `${index*100}%`;
+});
+
+let counter = 0;
+
+forwardButton.addEventListener('click', function(){
+    counter++;
+    carouselFunction();
+});
+
+backButton.addEventListener('click', function(){
+    counter--;
+    carouselFunction();
+});
+
+function carouselFunction(){
+
+    slides.forEach(function(slide){
+        slide.style.transform = `translateX(-${counter*50}%)`;
+    });
+}
+
+console.log(slides);
---
+++ spesificscript.js
@@ -0,0 +1,138 @@
+const detailPage = document.querySelector(".spesific_post_container");
+const imageResize = document.querySelector(".image_container");
+const mainUrl = "http://localhost/flower-power/wp-json/wp/v2/posts/";
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const id = params.get("id");
+console.log(id);
+
+detailPageUrl = mainUrl + id;
+console.log(detailPageUrl);
+detailPage.innerHTML= "";
+
+async function productDetails(){
+   try{
+       const fetchDetails = await fetch(detailPageUrl);
+       const details = await fetchDetails.json();
+       console.log(details);
+
+       createHtml(details);
+   }
+   catch(error){
+       console.log(error);
+   }
+   
+   
+}
+
+productDetails();
+
+function createHtml(details){
+   detailPage.innerHTML += `<h1>${details.title.rendered}</h1>
+                            ${details.content.rendered}
+                           `
+    imageResize.innerHTML += `${details.content.rendered}`
+    document.title +=   ` ${details.title.rendered}`;
+   
+}
+
+
+
+/*document.getElementsByClassName("img").style.transform = "scale(1,5)";*/
+/*addEventListener(click, ) 
+
+const image = document.querySelector(".images");
+console.log(image);*/
+
+
+/*
+
+const image = document.querySelector(".image_container");
+const mediaUrl = "http://localhost/flower-power/wp-json/wp/v2/media/";
+const imageQueryString = document.location.search;
+const imageParams = new URLSearchParams(imageQueryString);
+const imageId = params.get("id");
+console.log(imageId);
+
+imageUrl = mediaUrl + imageId;
+console.log(imageUrl);
+
+
+async function pictures(){
+   try{
+       const fetchPictures = await fetch(imageUrl);
+       const images = await fetchPictures.json();
+       console.log(images);
+       image.innerHTML= "";
+       getImages(images);
+   }
+   catch(error){
+       console.log(error);
+   }
+   
+}
+
+pictures();
+
+function getImages(images){
+   image.innerHTML += `<img src="${images.media_details.file}">
+                           `;
+   
+}
+*/
+
+/*
+const image = document.querySelector(".image_container");
+/*const mainUrl = "http://localhost/flower-power/wp-json/wp/v2/posts/";
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const id = params.get("id");
+console.log(id);
+
+detailPageUrl = mainUrl + id;
+console.log(detailPageUrl);
+image.innerHTML= "";
+
+async function images(){
+   try{
+       const fetchImages = await fetch(detailPageUrl);
+       const blogImages = await fetchImages.json();
+       console.log(blogImages);
+
+       createHtml(blogImages);
+   }
+   catch(error){
+       console.log(error);
+   }
+   
+   
+}
+
+images();
+
+function createHtml(blogImages){
+   image.innerHTML += ` <div class=".image" ${blogImages.content.rendered}<div>
+                           `;
+   
+}*/
+
+/*
+
+console.log(imageResize);
+imageResize.onclick = function (event){
+    imageResize.style.width = `100%`;
+    imageResize.style.height = `auto`;
+    imageResize.style.transition = `width 05.s ease`;
+} 
+
+document.onclick= function(){
+    imageResize.style.width = `80%`;
+    imageResize.style.height = `auto`;
+    imageResize.style.transition = `width 05.s ease`;
+
+}*/
+
+
+imageResize.addEventListener("click", function(){
+    this.classList.toggle("half");
+})
